772d358447320f2d7a0c5911bb19b2a7f101d52b
Pato2790 <pato.sartore@gmail.com>
2020-07-30 08:46:26 +0200

    Agregada libreria de angular-calendar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CalendarView, CalendarEvent } from 'angular-calendar';
+import { setHours, setMinutes } from 'date-fns';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'test-constant';
+  view: CalendarView = CalendarView.Week;
+
+  viewDate: Date = new Date();
+
+  events: CalendarEvent[] = [
+    {
+      title: 'No event end date',
+      start: setHours(setMinutes(new Date(), 0), 3),
+    },
+    {
+      title: 'No event end date',
+      start: setHours(setMinutes(new Date(), 0), 5),
+    },
+  ];
 }
